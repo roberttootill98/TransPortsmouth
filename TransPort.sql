e7b228b318e1440015b621eee0986533b13fb630
up804960 <36071498+up804960@users.noreply.github.com>
2018-02-12 15:59:08 +0000

    Add files via upload
    Initial Database, Barebones

---
+++ TransPort.sql
@@ -0,0 +1,46 @@
+create database TransPortsmouth;
+
+create table Establishment(
+est_Id int(8) not null,
+name varchar(60) not null,
+address varchar(150),
+town varchar(54),
+postcode varchar(8),
+description varchar (500)
+constraint PKest primary key (est_Id)
+);
+
+create table Resturant (
+resturant_Id int(8) not null,
+est_Id int(8) not null,
+theme varchar(20) not null,
+type varchar(20) not null
+constraint PKresturant primary key (resturant_Id)
+constraint FKestRest foreign key (est_Id) references Establishment(est_Id)
+);
+
+create table University (
+uni_Id int(8) not null,
+est_Id int(8) not null,
+canteen bool default False,
+free_computer bool default False
+constraint PKuni primary key (uni_Id)
+constraint FKestUni foreign key (est_Id) references Establishment(est_Id)
+);
+
+create table NightClub (
+club_Id int(8) not null,
+est_Id int(8) not null,
+costOfEntry double(2,2)
+constraint PKclub primary key (club_Id)
+constraint FKestClub foreign key (est_Id) references Establishment(est_Id)
+);
+
+create table Bar (
+bar_Id int(8) not null,
+est_Id int(8) not null,
+atomsphere varchar (20),
+type varchar (20)
+constraint PKbar primary key (bar_Id)
+constraint FKestBar foreign key (est_Id) references Establishment(est_Id)
+);
