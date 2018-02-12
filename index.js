1d86766875448b70cdd0a646eab462a7107cad5d
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-02-12 16:08:32 +0000

    js dom testing
    placing 'Max' in the HTML file with javascript

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 'use strict'
+
+let data = {
+  "name":"max",
+  "surname":"tune"
+}
+
+function boot(){
+
+  let test = document.getElementById('test');
+  let text = document.createElement("p");
+  text.textContent = data.name;
+  test.appendChild(text);
+
+
+}
+
+
+window.addEventListener("load", boot)
