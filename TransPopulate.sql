f5eaa9fb831b7ccf18d3b501b06760a0158f2284
up804960 <36071498+up804960@users.noreply.github.com>
2018-02-18 10:56:33 +0000

    Updated tables to work with new changes
    Edited inserts to work with new code changes.
    Added a Contact info entry for Las Iguanas Portsmouth

--- TransPopulate.sql
+++ TransPopulate.sql
@@ -1,9 +1,12 @@
 /*Inserting into Establishment*/
-insert into Establishment (Name, Phone, Address, Town, Postcode, Description) values ("Las Iguanas", "02392293797", "108-111 East Side Plaza, Gunwharf Quays", "Portsmouth", "PO1 3SY", "Flame grilled Latin American food and sharing dishes");
-insert into Establishment (Name, Phone, Address, Town, Postcode, Description) values ("Lion Gate Building", "02392846400", "Lion Terrace", "Portsmouth", "PO1 3HF", "University Building: primarily used by Computing and Mathmatics Students");
-insert into Establishment (Name, Phone, Address, Town, Postcode, Description) values ("The Astoria", "02392863741", "37-39 Guildhall Walk", "Portsmouth", "PO1 2RY", "Nightclub with VIP balcony, Offers two different dance floors with different musical preference");
-insert into Establishment (Name, Phone, Address, Town, Postcode, Description) values ("Honest Politician", "02392298877", "47 Elm Grove", "Portsmouth", "PO5 1JF", "A casual bar which offers cocktails, pool and darts");
-insert into Establishment (Name, Phone, Address, Town, Postcode, Description) values ("Lyberry", "02392851157", "29-33 Guildhall Walk", "Portsmouth", "PO1 2RY", "Bar / Nightclub connected to Astoria");
+insert into Establishment (Name, Address, Town, Postcode, Description) values ("Las Iguanas", "108-111 East Side Plaza, Gunwharf Quays", "Portsmouth", "PO1 3SY", "Flame grilled Latin American food and sharing dishes");
+insert into Establishment (Name, Address, Town, Postcode, Description) values ("Lion Gate Building", "Lion Terrace", "Portsmouth", "PO1 3HF", "University Building: primarily used by Computing and Mathmatics Students");
+insert into Establishment (Name, Address, Town, Postcode, Description) values ("The Astoria", "37-39 Guildhall Walk", "Portsmouth", "PO1 2RY", "Nightclub with VIP balcony, Offers two different dance floors with different musical preference");
+insert into Establishment (Name, Address, Town, Postcode, Description) values ("Honest Politician", "47 Elm Grove", "Portsmouth", "PO5 1JF", "A casual bar which offers cocktails, pool and darts");
+insert into Establishment (Name, Address, Town, Postcode, Description) values ("Lyberry", "29-33 Guildhall Walk", "Portsmouth", "PO1 2RY", "Bar / Nightclub connected to Astoria");
+
+/*Inserting into Contact*/
+insert into Contact_Info values (1, "023 9229 3797", "Gunwharf@iguanas.co.uk","https://www.iguanas.co.uk/restaurants/portsmouth", "https://www.facebook.com/LasIguanasGunwharfQuays/");
 
 /*Inserting into Time*/
 insert into Work_Time values (1, '10:00:00', '23:00:00', '10:00:00', '23:00:00', '10:00:00', '23:00:00', '10:00:00', '23:00:00', '10:00:00', '00:00:00', '10:00:00', '00:00:00', '10:00:00', '22:30:00');
