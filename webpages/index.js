49c507261035e4647e2b0f025ceb3704321ab92c
Robert Tootill <UP811906@uni.ds.port.ac.uk>
2018-02-23 14:37:54 +0000

    Added class to category
    class = category

--- webpages/index.js
+++ webpages/index.js
@@ -27,10 +27,7 @@ function useResponse(table) {
   for (let cat of categories) {
     let el = document.createElement("li");
     el.textContent = cat;
-<<<<<<< HEAD
-=======
-
->>>>>>> b320f0340e4b6ca0573f318dc43973af6dac9e16
+    el.classList.add("category");
     container.appendChild(el);
 
   }
