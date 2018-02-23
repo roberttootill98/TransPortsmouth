001e6b8e92a23cc85ff25651b96453374a474f96
up804960 <36071498+up804960@users.noreply.github.com>
2018-02-23 14:02:26 +0000

    Update query.sql

--- server/query.sql
+++ server/query.sql
@@ -6,3 +6,9 @@ SELECT Name, Address, Town
 FROM Establishment
 INNER JOIN Restaurant on Establishment.Est_Id = Restaurant.Est_Id;
 
+/*Selecting all important info for an establishment of type Restaurant*/
+SELECT Establishment.Name, Establishment.Address, Restaurant.Theme, Restaurant.Type, Contact_Info.Phone, Contact_Info.Website, Contact_Info.Facebook
+FROM Establishment
+INNER JOIN Restaurant on Establishment.Est_Id = Restaurant.Est_Id
+INNER JOIN Contact_Info on Establishment.Est_Id = Contact_Info.Est_Id
+WHERE Establishment.Est_Id = 1;
