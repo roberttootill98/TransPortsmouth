4159cda71b62b8adc743eaaa224d11ca6bb05f35
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-02-23 20:28:44 +0000

    delete redundant stuff

--- server/server.js
+++ server/server.js
@@ -3,14 +3,12 @@ const app = express();
 const fs = ('require');
 const db = require('./ModelSQL.js');
 
-app.get('/api/planner', updateTitle);
+
 app.get('/api/category', getAll);
 
 db.init();
 
-async function updateTitle(req, res) {
-  await db.newTitle(req.query.t)
-}
+
 
 async function getAll(req, res) {
   res.json(await db.showAll("University"));
