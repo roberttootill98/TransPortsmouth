1db41fe23ec3e827cac0a15a9e1535d73b09b214
up804960 <36071498+up804960@users.noreply.github.com>
2018-02-27 16:17:27 +0000

    Update query.sql

--- server/query.sql
+++ server/query.sql
@@ -1,12 +1,15 @@
 /*Example queries used for data retrieval */
-
+/*Gathering all the table categories */
+SELECT Category
+FROM Establishment
+GROUP BY Category
 
 /*Searching for all Restaurants*/
 SELECT Name, Address, Town
 FROM Establishment
 INNER JOIN Restaurant on Establishment.Est_Id = Restaurant.Est_Id;
 
-/*Selecting all important info for an establishment of type Restaurant*/
+/*Selecting all important info for an establishment of type Restaurant where RESTAURANT and 1 can be switched out for any other value*/
 SELECT Establishment.Name, Establishment.Address, Restaurant.Theme, Restaurant.Type, Contact_Info.Phone, Contact_Info.Website, Contact_Info.Facebook
 FROM Establishment
 INNER JOIN Restaurant on Establishment.Est_Id = Restaurant.Est_Id
