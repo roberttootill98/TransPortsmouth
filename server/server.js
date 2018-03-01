18e11b24d68ba00f4950e2bec16ffdca4e4a8c47
Robert Tootill <UP811906@uni.ds.port.ac.uk>
2018-03-01 14:26:51 +0000

    Added all basic server functions
    getCategories
    getCategory
    getEstablishment
    getReview
    postReview

--- server/server.js
+++ server/server.js
@@ -3,19 +3,48 @@ const app = express();
 const fs = ('require');
 const db = require('./ModelSQL.js');
 
+app.get('/api/categories', getCategories);
+app.get('/api/category', getCategory);
+app.get('/api/establishment', getEstab);
+app.get('/api/review', getReview);
+app.post('/api/review', postReview);
 
-app.get('/api/category', getAll);
-//app.get('/api/establishment', getEstab);
 db.init();
 
-async function getAll(req, res) {
+//static list of categories
+async function getCategories(req, res) {
+  res.json(categories);
+}
+
+//list of establishments of type category
+async function getCategory(req, res) {
   res.json(await db.showAll(req.query.cat));
 }
 
-//async function getEstab(req, res) {
-//  res.json(await db.showAll(req.query.est));
-//}
+//one category with full details and DIRECTIONS
+async function getEstablishment(req, res) {
+  res.json(await db.whereAll(req.query.establishment));
+}
+
+//list of reviews
+async function getReview(req, res) {
+  res.json(await db.showAll("review"));
+}
+
+//add one new review
+async function postReview(req, res) {
+  try {
+    await db.addReview(req.establishment, req.title, req.desc, req.author);
+    res.sendStatus(200);
+  } catch(e) {
+    console.error(e);
+    res.sendStatus(404);
+  }
+}
 
 app.listen(8080);
 
 app.use('/', express.static('webpages', { extensions: ['html'] }));
+
+//in-memory
+let categories = ["bar", "nightclub", "Restaurant", "Univeristy", "shop", "Cafe", "gym"]
