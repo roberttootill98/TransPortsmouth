7bdba8db6cc1e0c75eee9f5db2e84f39397e211f
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-03-05 23:09:58 +0000

    ""

--- server/ModelSQL.js
+++ server/ModelSQL.js
@@ -22,7 +22,7 @@ async function showAll(table) {
 }
 
 async function whereAll(table, condition, checkfield) {
-  const query = 'SELECT * FROM ' + table + ' WHERE ' + checkfield + ' = ' + condition;
+  const query = 'SELECT * FROM ' + table + ' WHERE ' + checkfield + ' = "' + condition + '"';
   console.log(query);
   const formattedQuery = sql.format(query);
   const rows = await sql.query(formattedQuery);
