d08ca18bd1f324cb0972eb8b5d6171cd50b833cb
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-03-06 15:55:02 +0000

    I'm really good at syntax

--- webpages/index.js
+++ webpages/index.js
@@ -96,7 +96,7 @@ async function getEstabDetails(e){
   const response = await fetch(url);
   let estabDetails = await response.json();
   console.log(estabDetails);
-  for (let detail of details){
+  for (let detail of estabDetail){
 
     console.log(detail.Name);
     console.log(detail.Address);
