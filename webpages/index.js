990275e658b8cfc84391bdc474e64d2a319de22a
Et2211 <32326286+Et2211@users.noreply.github.com>
2018-03-07 14:01:31 +0000

    test

--- webpages/index.js
+++ webpages/index.js
@@ -73,7 +73,7 @@ async function displayCategory(establishments) {
     let el = document.createElement("li");
     let name = await getEstabName(cat.Est_Id);
 
-    el.textContent = name;
+    el.textContent = cat.name;
     el.classList.add("category");
     container.appendChild(el);
     el.addEventListener("click", getEstabDetails);
