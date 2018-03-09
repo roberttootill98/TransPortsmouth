90df294546d3d36009883a1a6ce3cf233db6168b
Robert Tootill <UP811906@uni.ds.port.ac.uk>
2018-03-09 10:36:26 +0000

    Changed queries to use interpolation
    3 functions affected, also some console.log()s

--- server/ModelSQL.js
+++ server/ModelSQL.js
@@ -15,18 +15,14 @@ async function init() {
 }
 
 async function showAll(table) {
-  const query = 'SELECT * FROM ' + table;
+  const query = `SELECT * FROM ${table}`;
   const formattedQuery = sql.format(query);
   const rows = await sql.query(formattedQuery);
   return rows[0];
 }
 
 async function whereAll(table, condition, checkfield) {
-  console.log("table" + table);
-  console.log("condition" + condition);
-  console.log("checkfield" + checkfield);
-  const query = 'SELECT * FROM ' + table + ' WHERE ' + checkfield + ' = "' + condition + '"';
-  console.log(query + 'is a query');
+  const query = `SELECT * FROM ${table} WHERE ${checkfield} = "${condition}";`;
   const formattedQuery = sql.format(query);
   const rows = await sql.query(formattedQuery);
   return rows[0];
@@ -34,7 +30,7 @@ async function whereAll(table, condition, checkfield) {
 
 async function addReview(est_id, title, content, score) {
   const query = 'INSERT INTO review (Est_Id, Title, Content, Score)';
-  const values = ' VALUES (' + est_id + ', "' + title + '", "' + content + '", "' + score + '");';
+  const values = ` VALUES (${est_id}, "${title}", "${content}", "${score}");`;
   const insertQuery = query + values;
 
   const formattedQuery = sql.format(insertQuery);
@@ -45,10 +41,9 @@ async function showCategory(table) {
   const query = "SELECT Establishment.Name, Establishment.Address, Establishment.Town," +
   " Work_Time.Mon_Open, Work_Time.Mon_Close, Work_Time.Tue_Open,Work_Time.Tue_Close, Work_Time.Wed_Open, Work_Time.Wed_Close, Work_Time.Thu_Open, Work_Time.Thu_Close, Work_Time.Fri_Open, Work_Time.Fri_Close,Work_Time.Sat_Open, Work_Time.Sat_Close, Work_Time.Sun_Open, Work_Time.Sun_Close" +
   " FROM Establishment" +
-  " INNER JOIN " + table + " on Establishment.Est_Id = " + table + ".Est_Id" +
+  ` INNER JOIN ${table} on Establishment.Est_Id = ${table}.Est_Id` +
   " INNER JOIN Work_Time on Establishment.Est_Id = Work_Time.Est_Id;"
   const formattedQuery = sql.format(query);
-  console.log(formattedQuery);
   const rows = await sql.query(formattedQuery);
   return rows[0];
 }
