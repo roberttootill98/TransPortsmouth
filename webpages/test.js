3acefd61eb11260cb2b0ec5c126192f3b088b87e
Robert Tootill <UP811906@uni.ds.port.ac.uk>
2018-03-15 11:28:23 +0000

    basic complete tests for submitReview

--- webpages/test.js
+++ webpages/test.js
@@ -7,32 +7,47 @@ async function test() {
 
 //for review.html
 async function reviewTest() {
-  let result = [];
-  console.log("starting test");
+  let validResult = []; //stores status of all requests that should be valid
+  let invalidResult = []; //stores status of all requests that should be invalid
+  let working = true;
 
   //valid case
-  result[0] = await submitReviewTest("Me", "It's a Restaurant", 5);
+  validResult.push(await submitReviewTest("Me", "It's a Restaurant", 5));
+
+
 
   //invalid cases
+
   //type errors
-  result[1] = await submitReviewTest("Me", "It's a Restaurant", "difjsdjif");
+  invalidResult.push(await submitReviewTest("Me", "It's a Restaurant", "difjsdjif"));
 
   //boundary cases
   //inside boundary
-  result[2] = await submitReviewTest("Me", "It's a Restaurant", 0);
-  result[3] = await submitReviewTest("Me", "It's a Restaurant", 10);
+  validResult.push(await submitReviewTest("Me", "It's a Restaurant", 0));
+  validResult.push(await submitReviewTest("Me", "It's a Restaurant", 10));
   //outside boundary
-  result[4] = await submitReviewTest("Me", "It's a Restaurant", -1);
-  result[5] = await submitReviewTest("Me", "It's a Restaurant", 11);
+  invalidResult.push(await submitReviewTest("Me", "It's a Restaurant", -1));
+  invalidResult.push(await submitReviewTest("Me", "It's a Restaurant", 11));
 
   console.log("results:");
-  console.log(result);
+  console.log(validResult);
+  console.log(invalidResult);
 
-  /*
-  for(let res of results) {
-    if(res == )
+  for(let result of validResult) {
+    if(result != 200) {
+      working = false;
+      break;
+    }
   }
-  */
+
+  for(let result of invalidResult) {
+    if(result != 400) {
+      working = false;
+      break;
+    }
+  }
+
+  console.log(working);
 }
 
 async function submitReviewTest(auth, cont, scr) {
